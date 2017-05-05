f64e447186baab56eee715a1342cef0279b77fba
Madeliene Owen <madeliene.owen@gmail.com>
2017-05-05 15:30:13 -0700

    Added couple of routes and basic html files.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(require('volleyball'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 
 // nunjucks rendering boilerplate
 app.set('view engine', 'html');
@@ -16,11 +17,11 @@ nunjucks.configure('views', { noCache: true });
 
 app.use(require('./routes'));
 
-app.use((req, res, next) => {
-  const err = new Error('Not found');
-  err.status = 404;
-  next(err);
-});
+// app.use((req, res, next) => {
+//   const err = new Error('Not found');
+//   err.status = 404;
+//   next(err);
+// });
 
 app.use((err, req, res, next) => {
   console.log("OH NOOOOOOOOOO", err.message);
@@ -29,7 +30,7 @@ app.use((err, req, res, next) => {
 
 db.sync()
 .then(() => {
-  app.listen(3001, () => console.log("Listening on port 3001..."));
+  app.listen(3000, () => console.log("Listening on port 3000..."));
 })
 .catch(err => {
   console.log("Error starting app: ", err);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
+const Promise = require('bluebird');
+const fs = require('fs');
+const Hotel = require('../models/hotel.js');
+const Restaurant = require('../models/restaurant.js');
+const Activity = require('../models/activity.js');
 
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', function(req, res, next) {
+  var outerScopeContainer = {};
+  Hotel.findAll()
+  .then(function (dbHotels) {
+    outerScopeContainer.dbHotels = dbHotels;
+    return Restaurant.findAll();
+  })
+  .then(function (dbRestaurants) {
+    outerScopeContainer.dbRestaurants = dbRestaurants;
+    return Activity.findAll();
+  })
+  .then(function (dbActivities) {
+    res.render('index', {
+      templateHotels: outerScopeContainer.dbHotels,
+      templateRestaurants: outerScopeContainer.dbRestaurants,
+      templateActivities: dbActivities
+    });
+  })
+  .catch(next);  
+})
+
+// ex: accessing: http://localhost:3000/bootstrap/dist/css/bootstrap.min.css 
+router.get('/bootstrap/:filename', function(req, res, next) {
+  let filenameAndDir = req.params.filename;
+  let contents = fs.readFileSync('../node_modules/bootstrap/dist/', filenameAndDir); 
+  res.send(contents);
+})
 
 module.exports = router;
